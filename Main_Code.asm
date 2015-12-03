7526182a5606f870687df0a9622e2565844a2447
irunonpho <ducnguyen2000@gmail.com>
2015-12-03 07:40:00 -0600

    Update Main_Code.asm
    Main_Code.asm
    added scrambler, seed generator, random number generator, and modified main

--- Main_Code.asm
+++ Main_Code.asm
@@ -3,6 +3,13 @@
 # .data declarations from Nathan's portion
 fin: 			.asciiz				"compiledWordList4.txt"
 fin1:			.asciiz 			"byteData5.dat"	
+fin2: 			.asciiz				"Seeds.txt"
+#####################################################################
+SeedBuffer:		.space		9		#Seed Buffer
+Seed:			.word		0		#seed initialized to zero
+SeedCounter:		.word		0			
+Rand:			.word		0
+#####################################################################
 .align 0
 completeWordList:	.space		3900000
 .align 0
@@ -16,7 +23,6 @@ seedJumpTable:		.space		40000		#room for the offsets of 9 letter words in the wo
 .align 2
 wordListAddr: 		.space 		12		#1st word = new wordlist, 2nd word = FoundWordlist, 3rd word = FoundWordList pointer
 newLine: 		.asciiz		"\n"
-foundWordsString:	.asciiz 	"These are the words you found: "
 ####################################################################
 nl: .asciiz "\n"
 space: .asciiz "   "
@@ -31,7 +37,8 @@ badWordPrompt: .asciiz "This word isn't valid!\n"
 endTimePrompt: .asciiz "Sorry, you're out of time, "
 endingPrompt: .asciiz "Game Over!\nYour score is "
 fullWordPrompt: .asciiz "Your nine letter word was: "
-userInput: .space 11
+seedPrompt:	.asciiz "\nSeed #: "
+userInput: .space 10
 fullWord: .space 11
 letterString: .space 9
 middleLetter: .space 2
@@ -76,7 +83,7 @@ main:
 	#Actual game starts here
 UI:	jal UIPrint #subroutine to print the UI
 	la $a0, userInput #Get user input
-	la $a1, 11 #max input is 9
+	la $a1, 10 #max input is 9
 	li $v0, 8
 	syscall
 	
@@ -107,9 +114,11 @@ notMiddle: addi $a0, $a0, 1 #Increment pointer and counter
 counterEnd:
 	beqz $s2, badWord
 	li $a1, 0
-	addi $a1, $s1, 0		#load number of letters in argument to choose which file to check
-	jal find			#Calls function to check word, stores if found (1) or not(0) in $a1
-
+	addi $a1, $s1, 0 #load number of letters in argument to choose which file to check
+	jal find #Calls function to check word, stores if found (1) or not(0) in $a1
+	move $a0, $a1
+	li $v0, 1
+	syscall
 	beqz $a1, badWord #If not found, bad word
 	la $a0, goodWordPrompt #Else, print valid prompt and add points and seconds to timer
 	li $v0, 4
@@ -138,7 +147,51 @@ badWord:
 
 
 scramble: #Where the letters will randomize in order
-	
+	la $t6, letterString
+	la $t0, Seed	#load seed's address
+	lw $t1, ($t0)	#loads seed into registry
+	la $t0, SeedCounter	#loads seed counter
+	lw $t2, ($t0)	#loads seed counter into registry
+	addi $t2, $t2, 1 #updates counter scrambler
+	sw $t2, ($t0)
+	smodl1: #modular algorithm, divides based on counter
+	divu $t1, $t1, 9
+	subi $t2, $t2, 1
+	blt $t2, $zero, breakmodl
+	mfhi $t3
+	mul $t1, $t1, $t3
+	j smodl1
+	breakmodl:
+	mfhi $t4
+	la $t0, middleLetter #sets middle letter
+	add $t4, $t6, $t4
+	sb $t0, ($t4)
+	add $t2, $zero, 8
+	add $t3, $zero, 8
+	addi $t5, $zero, 1
+	smodl2:
+	beq $t3, $t2 midjump #swaps letters randomly
+	beq $t4,$t2, midjump
+	divu $t1, $t1, 3
+	mfhi $t4
+	bgt $t4, $t5, swap
+	swap:
+		add $t7, $t6, $t3
+		add $t8, $t6, $t2
+		lb $t9, ($t7)
+		sb $t8, ($t7)
+		sb $t8, ($t9)
+	midjump:
+	subi $t2, $t2, 1
+	blt $t2, $zero, breakmod2nested
+	j smodl2
+	breakmod2nested:
+	mul $t1, $t1, 20000
+	subi $t3, $t3, 1
+	blt $t3, $zero, breakmod2
+	j smodl2
+	breakmod2:
+	lw $ra, ($sp)
 	li $v0, 30 #Resume time
 	syscall
 	add $s0, $s0, $a0 
@@ -290,14 +343,34 @@ end:
 	syscall
 	la $a0, fullWord
 	syscall
-	la $a0, newLine
-	syscall
-	jal print_found_words
-	j exit
+	
 #############################################################################################################
 # FileInput code for inputing the byteCode data(9 letter indexs) and the wordlist
 #############################################################################################################
-fileInput:	li   $v0, 13       # system call for open file
+fileInput:	
+		#Loads seed data into completeWordList, to be overwritten once seed is obtained
+		li   $v0, 13       # system call for open file
+		la   $a0, fin2     # board file name
+		li   $a1, 0        # Open for reading
+		li   $a2, 0
+		syscall            # open a file (file descriptor returned in $v0)				
+		move $s6, $v0      # save the file descriptor 
+
+		#read from file
+		li   $v0, 14       # system call for read from file
+		move $a0, $s6      # file descriptor 
+		la   $a1, completeWordList     # address of buffer to which to read
+		li   $a2, 3900000     # hardcoded buffer length
+		syscall            # read from file
+		
+		# Close the file 
+		
+		jal SeedGenerator #Generates seed
+		
+		li   $v0, 16       # system call for close file
+		move $a0, $s6      # file descriptor to close
+		syscall            # close file
+		li   $v0, 13       # system call for open file
 		la   $a0, fin      # board file name
 		li   $a1, 0        # Open for reading
 		li   $a2, 0
@@ -345,31 +418,20 @@ fileInput:	li   $v0, 13       # system call for open file
 #Found words list (placed in the 2nd word of wordListAddr) if the word is found. If the word is not found, returns -1 in $v0
 ##############################################################################################################################################
 
-find:
-		la $t2, userInput
-		move $t5, $zero
-		li $t4, 10
-remove:		lb $t3, 0($t2)
-		addi $t2, $t2, 1
-		addi $t5, $t5, 1
-		bnez $t3, remove
-		beq $t4, $t5, skip
-		addi $t2, $t2, -2
-		sb $zero, 0($t2)
-skip:
-		
-		la $t1, wordListAddr
+find:		la $t1, wordListAddr
 		lw $t2, 0($t1)			#store the address of the new wordlist
 		la $t4, compareString
 		la $t3, userInput
 		add $t9, $zero, $zero
 		
 stringLenLoop:	lbu $t5, 0($t3)
-		beqz $t5, compareLoop
+		beqz $t5, removeNewline#compareLoop
 		addi $t3, $t3, 1			##this loop finds the # of letters in the userInputString
 		addi $t9, $t9, 1
+		
 		j stringLenLoop
-
+removeNewline:	sb $zero, -1($t3)
+		addi $t9, $t9 -1	
 compareLoop:	
 
 		la $t4, compareString
@@ -546,12 +608,6 @@ print_found_words:	la $t1, wordListAddr			#load the address of the array holding
 			la $t3, printString			#load the address of the string we are going to use to print the words with
 			lw $t1, 4($t1)				#load the base address of the found words list from the wordListAddr array
 			
-			la $a0, foundWordsString
-			li $v0, 4
-			syscall
-			la $a0, newLine
-			syscall
-			
 printLoop:		lbu $t2, 0($t1)				#load the next character in the foudn words list
 			beqz $t2, finishPrint			#if null, the list is complete and jump to the finish portion of this function
 			sb $t2, 0($t3)				#store the character into the 
@@ -580,4 +636,98 @@ nullPrintEnd:		la $t3, printString
 finishPrint:	jr $ra						#jump back to the caller
 			
 ##############################################################################################################################################	
+SeedGenerator:  #save registries not in use, okay to use
+			subi $sp,$sp, -4
+			sw $ra, 0($sp)
+			j gets
+	random:
+		move $a0, $zero
+		li $a1, 1841 #upper limit of random number
+		li $v0, 42   #random
+		syscall
+		sll $t0, $a0, 9 #multiplies 1841 by 9, the size of 8 bytes plus \n
+		jr $ra
+	gets:
+		la $23, completeWordList      #set base address of Seeds to s3
+		la $s1, SeedBuffer
+		jal random
+		add $s3, $s3, $a0
+	l1:	#loops til; beginning of integer
+		lb $t0, 0($s3)        #load the char from Seeds into t0
+		lb $t1, newLine     #load newline char into t1
+		beq $t0, $t1, l2 
+		addi $s3, $s3, 1    #increments base address of Seeds
+		j l1
+	l2:           #start of read loop
+		addi $s3, $s3, 1
+		lb $t0, 0($s3)      #Loads first character from Seeds
+		sb $t0, 0($s1)      #store the char into the seed
+		lb $t1, newLine     #load newline char into t1
+		beq $t0, $t1, done  #end of string
+		addi $s1, $s1, 1    #increments base address of array
+		j l2          #jump to start of l2
+
+	done:           #let s2 be the sum total
+		addi $s1, $s1, -1   #reposition array pointer to last char before newline char
+		la $s0, SeedBuffer       #set base address of array to s0 for use as counter
+		addi $s0, $s0, -1   #reposition base array to read leftmost char in string
+		add $s2, $zero, $zero   #initialize sum to 0
+		li $t0, 10      #set t0 to be 10, used for decimal conversion
+		li $t3, 1
+		lb $t1, 0($s1)      #load char from array into t1
+		blt $t1, 48, error  #check if char is not a digit (ascii<'0')
+		bgt $t1, 57, error  #check if char is not a digit (ascii>'9')
+		addi $t1, $t1, -48  #converts t1's ascii value to dec value
+		add $s2, $s2, $t1   #add dec value of t1 to sumtotal
+		addi $s1, $s1, -1   #decrement array address
+	lp:         #loop for all digits preceeding the LSB
+		mul $t3, $t3, $t0   #multiply power by 10
+		beq $s1, $s0, SGend   #exit if beginning of string is reached
+		lb $t1, ($s1)       #load char from array into t1
+		blt $t1, 48, error  #check if char is not a digit (ascii<'0')
+		bgt $t1, 57, error  #check if char is not a digit (ascii>'9')
+		addi $t1, $t1, -48  #converts t1's ascii value to dec value
+		mul $t1, $t1, $t3   #t1*10^(counter)
+		add $s2, $s2, $t1   #sumtotal=sumtotal+t1
+		addi $s1, $s1, -1   #decrement array address
+		j lp            #jump to start of loop
+
+	error:          #if non digit chars are entered, readInt returns 0
+		add $s2, $zero, $zero
+		j SGend
+
+	SGend:
+		li $v0, 4
+		la $a0, seedPrompt #prompt for seed number
+		syscall
+		li $v0, 1
+		add $a0, $s2, $zero
+		syscall 
+		la $t0, Seed
+		sw $s0, 0($t0)
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+		jr $ra
+RandomNumber:	#random number method
+		subi $sp,$sp, -4
+		sw $ra, 0($sp)
+		la $t0, Seed
+		lw $t1, ($t0)
+		la $t0, SeedCounter
+		lw $t2, ($t0)
+		la $t3, Rand
+		rl: #modular algorithm, divides based on counter
+		divu $t1, $t1, 9620
+		subi $t2, $t2, 1
+		blt $t2, $zero, breakrl
+		mfhi $t4
+		mul $t1, $t1, $t4
+		j rl
+		breakrl:
+		mfhi $t4
+		sw $t4, ($t3)
+		addi $sp, $sp, 4
+		jr $ra
 exit:
+		li $v0, 10      #ends Program
+		syscall
